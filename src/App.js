a2e67c8fe04196a08a37aea91b71d82bed7ee149
Algassimou <alkann10@outlook.fr>
2019-12-17 10:13:01 +0000

    App count , list link

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Hello extends React.Component {
+    render() {
+        const styles = { color: this.props.color };
+        return <h1>{this.props.icone}<a href={this.props.link} style={styles}> {this.props.title}</a></h1>
+
+    }
+}
+
+class Link extends React.Component {
+    render() {
+        let className = "link";
+        return (
+            <div className={className}>
+                <h4>Mes Resaux Sociaux </h4>
+                <Hello title="Google" link="https://www.google.fr/ " color="#EA4335" icone="Ⓜ" />
+                <Hello title="Twitter" link="https://www.twitter.com " color="#1DA1F2" icone="👬" />
+                <Hello title="Instagram" link="https://www.instagram.com/?hl=fr" color="#AA3888" icone="☝" />
+            </div>
+        );
+    }
+}
+
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 0, trop: '' };
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+
+    }
+
+    increment() {
+        this.state.count <= 9 && this.setState({ count: this.state.count + 1 })
+        this.state.count >= 2 && this.setState({ trop: "✅" });
+
+    }
+
+    decrement() {
+        this.state.count > 0 && this.setState({ count: this.state.count - 1 });
+        this.state.count <= 3 && this.setState({ trop: "😴 " });
+
+    }
+
+    render() {
+        let className = "count";
+        return (
+            <div className={className}>
+                <h1>Vous avez {this.state.count} point. {this.state.trop}</h1>
+                <div class='btn'>
+                    <button onClick={this.increment}>J'ai un nouveau point</button>
+                    <button onClick={this.decrement}>J'ai perdu un point</button>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Counter />
+                <Link />
+            </div>
+        );
+    }
 }
 
 export default App;
