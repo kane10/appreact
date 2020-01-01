78b016e6942e461c06a91aea9d81142b29a79bbd
Algassimou <alkann10@outlook.fr>
2020-01-01 13:51:55 +0000

    Updates

---
+++ precache-manifest.6fdfd478b868b0a994dac437d10bb319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8c377da6b66ce46b1dd4981ff616b7",
+    "url": "/appreact/index.html"
+  },
+  {
+    "revision": "69508cb61015b14c99ae",
+    "url": "/appreact/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "068561d6e462a71f9fd4",
+    "url": "/appreact/static/css/main.8711a2bf.chunk.css"
+  },
+  {
+    "revision": "69508cb61015b14c99ae",
+    "url": "/appreact/static/js/2.e1fd6dd5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/appreact/static/js/2.e1fd6dd5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "068561d6e462a71f9fd4",
+    "url": "/appreact/static/js/main.7b0b1d87.chunk.js"
+  },
+  {
+    "revision": "6a35d231711db9992a3d",
+    "url": "/appreact/static/js/runtime-main.bba39ee0.js"
+  }
+]);
